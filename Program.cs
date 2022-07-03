0b126f4e340d34a49db631d4bc221934968b9047
Dane Balia <dane.balia@standardbank.co.za>
2022-07-03 14:52:12 +0200

    Basic example

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+bool safe = true;
+
+while (safe)
+{
+
+    Process[] processes = Process.GetProcesses();
+
+    foreach (Process process in processes)
+    {
+        try
+        {
+            TSProcess p = new TSProcess() {
+                ProcessName = process.ProcessName,
+                PagedSystemMemorySize64 = process.PagedSystemMemorySize64 / 1024,
+                UserProcessorTime = process.UserProcessorTime.TotalSeconds,
+                FileName = process?.MainModule?.FileName
+            };
+
+            Console.WriteLine($"{process.ProcessName},{process.PagedSystemMemorySize64 / 1024},{process.UserProcessorTime},{process?.MainModule?.FileName}");
+        }
+        catch (System.Exception ex)
+        {
+            WriteColoredLine($"{ex.Message}: {process.ProcessName}", ConsoleColor.Red);
+        }
+    }
+
+    Thread.Sleep(1000);
+
+
+}
+
+
+static void WriteColoredLine(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
---
+++ TSProcess.cs
@@ -0,0 +1,7 @@
+class TSProcess
+{
+    public string ProcessName { get; set; }
+    public double PagedSystemMemorySize64 { get; set; }
+    public double UserProcessorTime { get; set; }
+    public string FileName { get; set; }
+}
