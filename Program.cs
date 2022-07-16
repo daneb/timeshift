2fbb83c0fb6c0f28646fdebd6d0c56c84a19421d
Dane Balia <dane.balia@standardbank.co.za>
2022-07-16 21:31:11 +0200

    Working tracking by way of time

--- Program.cs
+++ Program.cs
@@ -1,34 +1,90 @@
 ﻿using System.Diagnostics;
+using LiteDB;
 
 bool safe = true;
+Dictionary<string, double> processesSum = new Dictionary<string, double>(); 
 
 while (safe)
 {
 
-    Process[] processes = Process.GetProcesses();
-
-    foreach (Process process in processes)
+    using (var db = new LiteDatabase(@"C:\Temp\Timeshift.db"))
     {
-        try
-        {
-            TSProcess p = new TSProcess() {
-                ProcessName = process.ProcessName,
-                PagedSystemMemorySize64 = process.PagedSystemMemorySize64 / 1024,
-                UserProcessorTime = process.UserProcessorTime.TotalSeconds,
-                FileName = process?.MainModule?.FileName
-            };
+        var col = db.GetCollection<TSProcess>($"processes_{DateTime.Now.ToString("yyyyMMdd")}");
+        Process[] processes = Process.GetProcesses();
 
-            Console.WriteLine($"{process.ProcessName},{process.PagedSystemMemorySize64 / 1024},{process.UserProcessorTime},{process?.MainModule?.FileName}");
-        }
-        catch (System.Exception ex)
+        foreach (Process process in processes)
         {
-            WriteColoredLine($"{ex.Message}: {process.ProcessName}", ConsoleColor.Red);
+            try
+            {
+                TSProcess p = new TSProcess()
+                {
+                    Id = ObjectId.NewObjectId(),
+                    ProcessName = process.ProcessName,
+                    PagedSystemMemorySize64 = process.PagedSystemMemorySize64 / 1024,
+                    UserProcessorTime = process.UserProcessorTime.TotalSeconds,
+                    FileName = process?.MainModule?.FileName,
+                    TimeStamp = DateTime.Now.ToString("yyyy-MM-dd")
+                };
+
+                // Summarize time for each of the same process
+                if (processesSum.ContainsKey(p.ProcessName))
+                {
+                    processesSum[p.ProcessName] += p.UserProcessorTime;
+                }
+                else
+                {
+                    processesSum.Add(p.ProcessName, p.UserProcessorTime);
+                }
+
+                Console.WriteLine($"{process.ProcessName},{process.PagedSystemMemorySize64 / 1024},{process.UserProcessorTime},{process?.MainModule?.FileName}");
+                var existing = col.Find(x => x.FileName == p.FileName).FirstOrDefault();
+                if (existing == null)
+                {
+                    col.Insert(p);
+                }
+                else
+                {
+                    if (processesSum.ContainsKey(p.ProcessName))
+                        p.UserProcessorTime = processesSum[p.ProcessName];
+                    
+                    col.Update(existing.Id, p);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                WriteColoredLine($"{ex.Message}: {process.ProcessName}", ConsoleColor.Red);
+            }
         }
+
+        var processCollection = db.GetCollection<TSProcess>("process");
+        var result = processCollection.Query()
+        .ToEnumerable()
+        .Select(p =>
+        new
+        {
+            ProcessName = p.ProcessName,
+            PagedSystemMemorySize64 = p.PagedSystemMemorySize64 / 1024,
+            UserProcessorTime = p.UserProcessorTime,
+            FileName = p.FileName,
+            TimeStamp = p.TimeStamp
+        });
+
+        var totals = result.GroupBy(g => g.FileName).Select(x => new
+        {
+            Name = x.Key,
+            Total = x.Sum(s => s.UserProcessorTime)
+        }).OrderBy(o => o.Total);
+
+        foreach (var item in totals)
+        {
+            WriteColoredLine($"Process: {item.Name} {item.Total}", ConsoleColor.Green);
+        }
+
     }
 
+    safe = false;
+
     Thread.Sleep(1000);
-
-
 }
 
 
--- TSProcess.cs
+++ TSProcess.cs
@@ -1,7 +1,12 @@
+using LiteDB;
+
 class TSProcess
 {
+    public ObjectId Id { get; set; }
     public string ProcessName { get; set; }
     public double PagedSystemMemorySize64 { get; set; }
     public double UserProcessorTime { get; set; }
     public string FileName { get; set; }
+
+    public string TimeStamp { get; set; }
 }
